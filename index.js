9dc70a1a200d73a7de6d97d81109e9425994d57d
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-07-11 13:43:02 +0530

    sac
    csc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World v2!')
+  res.send('Hello World v2 channy!')
 })
 
 app.get('/status', (req, res) => {
