de8a6ecf87c0eb709b3bbc0de667e675e702cdfe
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-07-11 13:50:28 +0530

    changes applied
    changes applied

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World v2 channy!')
+  res.send('Hello World v2 channy now you are visible!')
 })
 
 app.get('/status', (req, res) => {
