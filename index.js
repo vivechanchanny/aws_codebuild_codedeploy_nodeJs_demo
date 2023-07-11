7acd43a6b89a44d11c3cad6c64f53ad996f27744
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-07-11 14:58:25 +0530

    v3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World v2 channy now you are visible now!')
+  res.send('Hello World v2 channy now you are visible now! version ')
 })
 
 app.get('/status', (req, res) => {
